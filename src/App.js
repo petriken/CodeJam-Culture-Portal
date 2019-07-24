7d04738ce2e219934d09f738456e18d3e77cfbc7
Irina <irbris@mail.ru>
2019-07-24 15:22:15 +0300

    feat: personalPage structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import PersonalPage from './containers/PersonalPage/PersonalPage'
+import React, {Component} from 'react';
+import Header from './components/Header/Header';
+import CommonPage from './containers/CommonPage/CommonPage';
+import PersonalPage from './containers/PersonalPage/PersonalPage';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <PersonalPage/>
-    </div>
-  );
+export default class App extends Component {
+  state={
+    person: 0
+  }
+  onButtonClick = (event) => {
+    this.setState({
+        person: event.target.id
+      })
+  }
+  
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header/>
+          <Route path="/" 
+                 render={(props) => <CommonPage 
+                 onButtonClick={this.onButtonClick}/>} 
+                 exact/>
+          <Route path="/personalpage" 
+                 render={(props) => <PersonalPage person={this.state.person}/>} 
+                 exact/>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
