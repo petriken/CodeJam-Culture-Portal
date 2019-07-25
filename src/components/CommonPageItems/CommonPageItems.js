3c85f16dc184ba3c29cba26e7f1bd89d6d4dace9
AlexSkir <a.skirnevskaia@gmail.com>
2019-07-25 03:17:45 +0300

    feat: connect state store to app

--- src/components/CommonPageItems/CommonPageItems.js
+++ src/components/CommonPageItems/CommonPageItems.js
@@ -1,38 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './CommonPageItems.css';
 import Avatar from '../../components/Avatar/Avatar';
 import Description from '../../components/Description/Description';
 import data from '../../data/people';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function CommonPageItems(props) { 
-  const term = '';
-  
-  function search(arr, searchWord) {
-    return arr.filter((item) => item.name.indexOf(searchWord) > 0)
+class CommonPageItems extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ''
+    }
+  }
+
+  componentWillMount() {
+    this.setState({ items: data });
+  }
+
+  checkMatch(item, searchWord) {
+    let string = item;
+    let array;
+    if (string.indexOf('.') !== -1) {
+      array = string.split('.');
+    } else {
+      array = string.split(' ');
+    }
+    return array ? array.some(i => i.match(new RegExp(`^${searchWord}`, 'i'))) : ''
   };
-  
-  const visibleItems = term === '' ? data : search(data, term);
-  
-  return (    
-    visibleItems.map((item, index) => {
-      return (
-        <div className="common-page"
-             key={index}>          
-          <Avatar data={item}/>
-          <div className="description-container">
-            <Description data={item}/>
-            <Link to="/personalpage"  
-                  className="read-more-button"
-                  id={index}
-                  onClick={props.onButtonClick}>
-                  Узнать больше
-            </Link>        
+
+  render() {
+    return (
+      this.state.items.map((item, index) => {
+        const term = this.props.term ? this.props.term : '';
+        const city = this.props.city ? this.props.city : '';
+        return (
+          <div className={`common-page 
+          ${this.checkMatch(item.name, term) && this.checkMatch(item.birthPlace, city) ? 'shown' : 'hidden'}`}
+            key={index}>
+            <Avatar data={item} />
+            <div className="description-container">
+              <Description data={item} />
+              <Link to="/personalpage"
+                className="read-more-button"
+                id={index}
+                onClick={this.props.onButtonClick}>
+                Узнать больше
+            </Link>
+            </div>
           </div>
-        </div>
-      );
-    })
-  )
+        );
+      })
+    )
+  }
 }
 
-export default CommonPageItems;
+const mapStateToProps = state => ({ term: state.term, city: state.city })
+export default connect(mapStateToProps)(CommonPageItems);
