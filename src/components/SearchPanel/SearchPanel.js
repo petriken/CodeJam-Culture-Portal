4415077bdf6643be8f77e6b1c2c7aad1dc41986f
AlexSkir <a.skirnevskaia@gmail.com>
2019-07-25 03:22:42 +0300

    fix: fix search pannel

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,41 +1,61 @@
 import React, { Component } from 'react';
 import './SearchPanel.css';
+import store from '../../store/store';
 
 export default class SearchPanel extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      term: '',
+      city: ''
+    };
+  }
 
-  state = {
-    term: ''
-  };
-
-  onTermChange = (event) => {
-    const {onSearchChange = () => {}} = this.props;
+  onTermChange(event) {
     this.setState({
       term: event.target.value
     });
-    onSearchChange(event.target.value);
-  };
+    store.dispatch({ type: 'term', value: event.target.value });
+  }
+
+  onCityChange(event) {
+    this.setState({
+      city: event.target.value
+    });
+    store.dispatch({ type: 'city', value: event.target.value });
+  }
+
+  onClickHandler() {
+    const term = document.getElementById('termInput').value;
+    const city = document.getElementById('cityInput').value;
+    this.setState({ term, city })
+    store.dispatch({ type: 'term', value: term });
+    store.dispatch({ type: 'city', value: city });
+  }
 
   render() {
     return (
       <div className="search-container">
         <form className="search-panel">
-        <label>        
-          Я ищу фотографа
+          <label>
+            Я ищу фотографа
           <input type="text"
-                 className="search-input"
-                 placeholder="search"
-                 value={this.state.term}
-                 onChange={ this.onTermChange } />
-        </label>
-        <label>        
-          в городе
+              className="search-input"
+              placeholder="search"
+              id="termInput"
+              value={this.state.term}
+              onChange={e => this.onTermChange(e)} />
+          </label>
+          <label>
+            в городе
           <input type="text"
-                 className="search-input"
-                 placeholder="search"
-                 value={this.state.term}
-                 onChange={ this.onTermChange } />
-        </label>
-        <button className="btn-search" type="submit">Найти</button>
+              className="search-input"
+              placeholder="search"
+              id="cityInput"
+              value={this.state.city}
+              onChange={e => this.onCityChange(e)} />
+          </label>
+          <button className="btn-search" type="button" onClick={() => this.onClickHandler()}>Найти</button>
         </form>
       </div>
     );
