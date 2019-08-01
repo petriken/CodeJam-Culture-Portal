2ee9986081f0430cbaaab8befdb631f1f54259f3
AlexSkir <a.skirnevskaia@gmail.com>
2019-08-02 02:08:41 +0300

    delete cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('D:/gtsbyTest/gatsbyNetlify/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('D:/gtsbyTest/gatsbyNetlify/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
