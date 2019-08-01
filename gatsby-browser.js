d646a689240c6dda8752f409bf295c9eec1f47cb
AlexSkir <a.skirnevskaia@gmail.com>
2019-08-02 02:53:28 +0300

    rebuild

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { addLocaleData } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import be from 'react-intl/locale-data/be';
+import ru from 'react-intl/locale-data/ru';
+import ConnectedIntlProvider from './src/ConnectedIntlProvider';
 
 import store from './src/store/store';
 
+addLocaleData([...en, ...be, ...ru]);
+
 export const wrapRootElement = ({ element }) => {
   return (
-    <Provider store={store}>{element}</Provider>
+    <Provider store={store}><ConnectedIntlProvider>{element}</ConnectedIntlProvider></Provider>
   );
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,13 @@
 import React from "react";
-import { addLocaleData } from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import be from 'react-intl/locale-data/be';
-import ru from 'react-intl/locale-data/ru';
-import ConnectedIntlProvider from '../ConnectedIntlProvider';
 
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 
-addLocaleData([...en, ...be, ...ru]);
 
 export default ({ children }) => (
-  <ConnectedIntlProvider>
-    <>
-      <Header />
-      {children}
-      <Footer />
-    </>
-  </ConnectedIntlProvider>
+  <>
+    <Header />
+    {children}
+    <Footer />
+  </>
 );
