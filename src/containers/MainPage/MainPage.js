a96b547c40204fe5b10a23b50f5a4e49b04ed425
petriken <petriken82@gmail.com>
2019-08-03 02:22:16 +0300

    feat: add button to authorDay

--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './MainPage.css';
+import { Link } from 'react-router-dom';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { FormattedMessage, FormattedHTMLMessage } from 'react-intl';
+import { Button, Grid } from '@material-ui/core';
 import SimpleSlider from '../../components/Slider/Slider';
 import Avatar from '../../components/Avatar/Avatar';
 import Description from '../../components/Description/Description';
@@ -18,6 +20,7 @@ import avatarPetriken from '../../img/developers/petriken.jpg';
 import avatarIrinainina from '../../img/developers/irinainina.jpg';
 import avatarAlexSkir from '../../img/developers/AlexSkir.jpg';
 import avatarJulanick from '../../img/developers/Julanick.jpg';
+import store from '../../store/store';
 
 function MainPage(props) {
   let data;
@@ -29,78 +32,127 @@ function MainPage(props) {
     data = be;
   }
 
-
   function getProfileRand() {
     const profileRand = Math.random() * 8;
     return Math.floor(profileRand);
   }
-
   const profile = data[getProfileRand()];
+  console.log(profile);
+
   return (
-    <>
-      <div className="main-page"
-        id="home">
-        <div className="main-page-title">
+    <Grid>
+      <Grid className="main-page" id="home">
+        <Grid className="main-page-title">
           <p>
-            <span className="portal"><FormattedMessage id="headerSubtitleSpan" /></span>
-            <span><FormattedMessage id="headerSubtitle" /></span>
+            <span className="portal">
+              <FormattedMessage id="headerSubtitleSpan" />
+            </span>
+            <span>
+              <FormattedMessage id="headerSubtitle" />
+            </span>
           </p>
-          <div className="mainpage-navigation-container">
-            <AnchorLink
-              offset='40px'
-              href='#author'>
+          <Grid className="mainpage-navigation-container">
+            <AnchorLink offset="40px" href="#author">
               <FormattedMessage id="todayAuthor" />
             </AnchorLink>
-            <AnchorLink
-              offset='40px'
-              href='#project-info'>
+            <AnchorLink offset="40px" href="#project-info">
               <FormattedMessage id="infoAboutPortal" />
             </AnchorLink>
-            <AnchorLink
-              offset='0px'
-              href='#developers'>
+            <AnchorLink offset="0px" href="#developers">
               <FormattedMessage id="developers" />
             </AnchorLink>
-          </div>
-        </div>
+          </Grid>
+        </Grid>
         <SimpleSlider />
         <MainPageNavigation />
         <section className="author-day" id="author">
           <h2 className="title-author-day">
             <FormattedMessage id="todayAuthor" />
           </h2>
-          <div className="">
+          <Grid className="">
             <Avatar data={profile} />
             <Description data={profile} />
-          </div>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Button variant="contained" className="author-day__btn">
+              <FormattedMessage id="toAuthorPage">
+                {text => (
+                  <Link
+                    to={`/${props.lang}/personalpage/person${profile.id}`}
+                    className="author-day__btn-text"
+                    id={profile.id}
+                    onClick={(e) => {
+                      store.dispatch({
+                        type: 'page',
+                        value: `/${props.lang}/personalpage/person${profile.id}`,
+                      });
+                      localStorage.setItem(
+                        'page',
+                        `/${props.lang}/personalpage/person${profile.id}`,
+                      );
+                      props.onButtonClick(e);
+                    }}
+                  >
+                    {text}
+                  </Link>
+                )}
+              </FormattedMessage>
+            </Button>
+          </Grid>
         </section>
-        <section className="description-mainpage"
-          id="project-info">
+        <section className="description-mainpage" id="project-info">
           <h2>
             <FormattedMessage id="infoAboutPortal" />
           </h2>
           <FormattedHTMLMessage id="welcome" />
           <GridGallery data={profile} />
         </section>
-      </div>
-      <div className="developers-container"
-        id="developers">
-        <h2><FormattedMessage id="developers" /></h2>
+      </Grid>
+      <Grid className="developers-container" id="developers">
+        <h2>
+          <FormattedMessage id="developers" />
+        </h2>
         <FormattedMessage id="purpose" />
-        <div className="developers">
-          <Developer name="Vitaly Mikulich" gitHub="VitalyMikulich" avatar={avatarVitalyMikulich} />
-          <Developer name="Piotr Stashukevich" gitHub="petriken" avatar={avatarPetriken} />
-          <Developer name="Irina Inina" gitHub="irinainina" avatar={avatarIrinainina} />
-          <Developer name="Aleksandra Skirnevskaia" gitHub="AlexSkir" avatar={avatarAlexSkir} />
-          <Developer name="Yuliya Pakidzka" gitHub="Julanick" avatar={avatarJulanick} />
-        </div>
-      </div>
-    </>
+        <Grid className="developers">
+          <Developer
+            name="Vitaly Mikulich"
+            gitHub="VitalyMikulich"
+            avatar={avatarVitalyMikulich}
+          />
+          <Developer
+            name="Piotr Stashukevich"
+            gitHub="petriken"
+            avatar={avatarPetriken}
+          />
+          <Developer
+            name="Irina Inina"
+            gitHub="irinainina"
+            avatar={avatarIrinainina}
+          />
+          <Developer
+            name="Aleksandra Skirnevskaia"
+            gitHub="AlexSkir"
+            avatar={avatarAlexSkir}
+          />
+          <Developer
+            name="Yuliya Pakidzka"
+            gitHub="Julanick"
+            avatar={avatarJulanick}
+          />
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
 MainPage.propTypes = {
   lang: PropTypes.string.isRequired,
+  onButtonClick: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({ lang: state.locales.lang });
