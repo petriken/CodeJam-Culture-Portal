d30513f722e62c1d936f9d7122c3ebb7c5c5a118
AlexSkir <a.skirnevskaia@gmail.com>
2019-08-04 19:39:42 +0300

    fix conflict

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from "gatsby";
 import PropTypes from 'prop-types';
@@ -21,123 +21,134 @@ import Layout from "../components/layout";
 import { people } from '../data';
 import store from '../store/store';
 
-function MainPage(props) {
-  const data = people[props.lang];
-  let authorDay;
+class MainPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: ''
+    };
+    this.data = people[this.props.lang];
+    this.authorDay = ''
+  }
 
-  function getProfileRand() {
+  componentWillMount() {
+    this.setState({ profile: this.data[this.getProfileRand()] })
+  }
+
+  getProfileRand() {
     const profileRand = Math.random() * 8;
-    authorDay = Math.floor(profileRand);
+    this.authorDay = Math.floor(profileRand);
     return Math.floor(profileRand);
   }
-  // typeof localStorage !== 'undefined' ? localStorage.setItem('person', authorDay)
-  const profile = data[getProfileRand()];
-  return (
-    <Layout>
-      <Grid>
-        <Grid className="main-page" id="home">
-          <Grid className="main-page-title">
-            <p>
-              <span className="portal">
-                <FormattedMessage id="headerSubtitleSpan" />
-              </span>
-              <span>
-                <FormattedMessage id="headerSubtitle" />
-              </span>
-            </p>
-            <Grid className="mainpage-navigation-container">
-              <AnchorLink offset="40px" href="#author">
-                <FormattedMessage id="todayAuthor" />
-              </AnchorLink>
-              <AnchorLink offset="40px" href="#project-info">
-                <FormattedMessage id="infoAboutPortal" />
-              </AnchorLink>
-              <AnchorLink offset="0px" href="#developers">
-                <FormattedMessage id="developers" />
-              </AnchorLink>
-            </Grid>
-          </Grid>
-          <SimpleSlider />
-          <MainPageNavigation />
-          <section className="author-day" id="author">
-            <h2 className="title-author-day">
-              <FormattedMessage id="todayAuthor" />
-            </h2>
-            <Grid className="avatar-description">
-              <Avatar data={profile} />
-              <Grid>
-                <Description data={profile} />
-                <Grid
-                  container
-                  direction="row"
-                  justify="flex-start"
-                  alignItems="center"
-                  className="author-day-button"
-                >
-                  <Button variant="contained" className="author-day__btn">
-                    <FormattedMessage id="toAuthorPage">
-                      {text => (
-                        <Link
-                          to={`/photographers/person${authorDay}`}
-                          className="author-day__btn-text"
-                          id={authorDay}
-                          onClick={(e) => {
-                            store.dispatch({ type: 'person', value: authorDay });
-                          }}
-                        >
-                          {text}
-                        </Link>
-                      )}
-                    </FormattedMessage>
-                  </Button>
-                </Grid>
+
+  render() {
+    return (
+      <Layout>
+        <Grid>
+          <Grid className="main-page" id="home">
+            <Grid className="main-page-title">
+              <p>
+                <span className="portal">
+                  <FormattedMessage id="headerSubtitleSpan" />
+                </span>
+                <span>
+                  <FormattedMessage id="headerSubtitle" />
+                </span>
+              </p>
+              <Grid className="mainpage-navigation-container">
+                <AnchorLink offset="40px" href="#author">
+                  <FormattedMessage id="todayAuthor" />
+                </AnchorLink>
+                <AnchorLink offset="40px" href="#project-info">
+                  <FormattedMessage id="infoAboutPortal" />
+                </AnchorLink>
+                <AnchorLink offset="0px" href="#developers">
+                  <FormattedMessage id="developers" />
+                </AnchorLink>
               </Grid>
             </Grid>
-          </section>
-          <section className="description-mainpage" id="project-info">
+            <SimpleSlider />
+            <MainPageNavigation />
+            <section className="author-day" id="author">
+              <h2 className="title-author-day">
+                <FormattedMessage id="todayAuthor" />
+              </h2>
+              <Grid className="avatar-description">
+                <Avatar data={this.state.profile} />
+                <Grid>
+                  <Description data={this.state.profile} />
+                  <Grid
+                    container
+                    direction="row"
+                    justify="flex-start"
+                    alignItems="center"
+                    className="author-day-button"
+                  >
+                    <Button variant="contained" className="author-day__btn">
+                      <FormattedMessage id="toAuthorPage">
+                        {text => (
+                          <Link
+                            to={`/photographers/person${this.authorDay}`}
+                            className="author-day__btn-text"
+                            id={this.authorDay}
+                            onClick={(e) => {
+                              store.dispatch({ type: 'person', value: this.authorDay });
+                            }}
+                          >
+                            {text}
+                          </Link>
+                        )}
+                      </FormattedMessage>
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </section>
+            <section className="description-mainpage" id="project-info">
+              <h2>
+                <FormattedMessage id="infoAboutPortal" />
+              </h2>
+              <FormattedHTMLMessage id="welcome" />
+              <GridGallery data={this.state.profile} />
+            </section>
+          </Grid>
+          <Grid className="developers-container" id="developers">
             <h2>
-              <FormattedMessage id="infoAboutPortal" />
+              <FormattedMessage id="developers" />
             </h2>
-            <FormattedHTMLMessage id="welcome" />
-            <GridGallery data={profile} />
-          </section>
-        </Grid>
-        <Grid className="developers-container" id="developers">
-          <h2>
-            <FormattedMessage id="developers" />
-          </h2>
-          <FormattedMessage id="purpose" />
-          <Grid className="developers">
-            <Developer
-              name="Vitaly Mikulich"
-              gitHub="VitalyMikulich"
-              avatar={avatarVitalyMikulich}
-            />
-            <Developer
-              name="Piotr Stashukevich"
-              gitHub="petriken"
-              avatar={avatarPetriken}
-            />
-            <Developer
-              name="Irina Inina"
-              gitHub="irinainina"
-              avatar={avatarIrinainina}
-            />
-            <Developer
-              name="Aleksandra Skirnevskaia"
-              gitHub="AlexSkir"
-              avatar={avatarAlexSkir}
-            />
-            <Developer
-              name="Yuliya Pakidzka"
-              gitHub="Julanick"
-              avatar={avatarJulanick}
-            />
+            <FormattedMessage id="purpose" />
+            <Grid className="developers">
+              <Developer
+                name="Vitaly Mikulich"
+                gitHub="VitalyMikulich"
+                avatar={avatarVitalyMikulich}
+              />
+              <Developer
+                name="Piotr Stashukevich"
+                gitHub="petriken"
+                avatar={avatarPetriken}
+              />
+              <Developer
+                name="Irina Inina"
+                gitHub="irinainina"
+                avatar={avatarIrinainina}
+              />
+              <Developer
+                name="Aleksandra Skirnevskaia"
+                gitHub="AlexSkir"
+                avatar={avatarAlexSkir}
+              />
+              <Developer
+                name="Yuliya Pakidzka"
+                gitHub="Julanick"
+                avatar={avatarJulanick}
+              />
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
 
 MainPage.propTypes = {
