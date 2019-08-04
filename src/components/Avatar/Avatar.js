ecb24dd3d7fd870b3270710694abb079a45aabeb
AlexSkir <a.skirnevskaia@gmail.com>
2019-08-04 16:08:20 +0300

    fix: fix personal page's media selection

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -3,6 +3,7 @@ import './Avatar.css';
 import PropTypes from 'prop-types';
 
 function Avatar(props) {
+  console.log(props.data)
   return (
     <div className="avatar-container" id="avatar">
       <img src={props.data.avatar} alt={props.data.name} />
